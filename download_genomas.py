11efdf1b5d036164f5790122f8eccc70957cf414
baromagnoli <barbara.romagnoli@usp.br>
2025-02-12 13:48:21 -0300

    funcao de download dos genomas

--- download_genomas.py
+++ download_genomas.py
@@ -3,7 +3,7 @@ import os  # To navigate through system directories
 import click  # Library to create command-line commands
 
 @click.command()
-@click.option('--taxon-id', prompt='Enter the taxon ID (e.g., 1883)',
+@click.option('--taxon-id', prompt='Enter the taxon ID (1883)',
               help='The taxon ID to be used in the command.')
 @click.option('--reference', is_flag=True, help='Flag to indicate the use of reference.')
 @click.option('--annotated', is_flag=True, help='Flag to indicate the use of annotated data.')
@@ -11,8 +11,7 @@ import click  # Library to create command-line commands
               help='Directory where the download will be saved.')
 @click.option('--folder-name', prompt='Enter the folder name for the download (e.g., "download_folder")',
               help='Name of the folder where the files will be saved.')
-#click.option('--filename', prompt='Enter custom file name .zip',help='The custom file name')
-#--filename string  Specify a custom file name for the downloaded data package (default "ncbi_dataset.zip") < adicionar para fazer o download .zip
+
 def run_datasets_summary(taxon_id, reference, annotated, output_dir, folder_name):
     """
     It asks for the folder name and location where the download will be saved.
@@ -30,8 +29,7 @@ def run_datasets_summary(taxon_id, reference, annotated, output_dir, folder_name
         os.makedirs(download_path)
 
     # Build the command
-    command = ['./datasets', 'summary', 'genome', 'taxon', str(taxon_id), '--annotated', '--reference'] #summary apenas para teste, vou trocar por download
-
+    command = ['./datasets', 'download', 'genome', 'taxon', str(taxon_id), '--annotated', '--reference']
     # Add flags based on user input
     if annotated:
         command.append('--annotated')  # Ensure '--annotated' is added if the user chooses it
@@ -54,4 +52,3 @@ def run_datasets_summary(taxon_id, reference, annotated, output_dir, folder_name
 
 if __name__ == '__main__':
     run_datasets_summary()
-
