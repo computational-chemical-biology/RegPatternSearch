1a5f5c41bd4f3c207f817d32e6cebde7f5b66e71
baromagnoli <barbara.romagnoli@usp.br>
2025-04-30 09:44:09 -0300

    Updated

--- rodarNEXTFLOW.sh
+++ rodarNEXTFLOW.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=antismash_nextflow
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=20
 #SBATCH --mem=32G
 #SBATCH --time=48:00:00
 #SBATCH --output=nextflow_%j.out 
 #SBATCH --error=nextflow_%j.err
 
-##AJUSTAR PARAMETROS ACIMA ^
 
 module load nextflow
 nextflow run main.nf -resume
